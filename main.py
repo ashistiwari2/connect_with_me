bc2c65e1ef3a013249f5e82c4453471be41ef373
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-09-27 16:23:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-im = Image.open("favicon1.ico")
+im = Image.open("favicon2.ico")
 PAGE_TITLE = "Connect with Ashis Tiwari"
 st.set_page_config(page_title=PAGE_TITLE, page_icon=im)
 
